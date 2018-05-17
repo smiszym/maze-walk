950a367a46b3079aa88101e0e75acf097a41207a
Michał Szymański <smiszym@gmail.com>
2018-05-17 10:09:24 +0200

    Implement game - moving inside maze

---
+++ game.py
@@ -0,0 +1,40 @@
+import curses
+
+from mazewalk.maze import Maze
+from mazewalk.generators import random_generator
+from mazewalk.printers import utf8_printer
+
+
+def main(stdscr):
+    width = 39
+    height = 23
+    m = Maze(width, height, random_generator, utf8_printer)
+    pos = m.initial_position
+    x = pos[0]*2
+    y = pos[1]
+
+    # Draw the maze
+    stdscr.clear()
+    stdscr.addstr(str(m))
+
+    while True:
+        stdscr.move(y, x)
+        c = stdscr.getch()
+        if c == curses.KEY_LEFT:
+            if x > 0 and not m.get_wall((x-1)//2, y):
+                x -= 1
+        elif c == curses.KEY_RIGHT:
+            if x//2 < width - 1 and not m.get_wall((x+1)//2, y):
+                x += 1
+        elif c == curses.KEY_UP:
+            if y > 0 and not m.get_wall(x//2, y-1):
+                y -= 1
+        elif c == curses.KEY_DOWN:
+            if y < height - 1 and not m.get_wall(x//2, y+1):
+                y += 1
+        elif c == 27:
+            break  # quit the game
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
